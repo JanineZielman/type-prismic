10d253dbedd7582fee33975af44a3ee8cc5018b9
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-04-09 12:34:27 +0200

    build

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,8 @@ export default function Document() {
       <Head>
         <script language="javascript" type="text/javascript" src="/p5.min.js" async></script>
         <script language="javascript" type="text/javascript" src="/grid_drawing.js" async></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://sharonchoong.github.io/svg-exportJS/svg-export.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" async></script>
+        <script src="https://sharonchoong.github.io/svg-exportJS/svg-export.min.js" async></script>
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
